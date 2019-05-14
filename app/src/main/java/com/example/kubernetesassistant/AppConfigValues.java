39eaae6bca0dc81384d6f445272ead0d6dadb0ae
Ashutosh Nath Agarwal <ash7594@gmail.com>
2019-05-14 13:02:02 -0700

    refactor

--- app/src/main/java/com/example/kubernetesassistant/AppConfigValues.java
+++
@@ -1,7 +0,0 @@
-package com.example.kubernetesassistant;
-
-public class AppConfigValues {
-    public static final String SERVER_URL = "http://169.48.179.82:8080";
-    public static final String CLIENT_ID = "cAVmkwey2H";
-    public static final String CLIENT_SECRET = "Atv71LPULauz";
-}
