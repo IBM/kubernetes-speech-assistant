9cd3324452a523cfd164f7334fc2b541a3867a84
Ashutosh Nath Agarwal <ash7594@gmail.com>
2019-06-12 15:26:40 -0700

    remove file

--- app/src/main/java/com/example/kubernetesassistant/AppConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.kubernetesassistant;
-
-public class AppConfig {
-    public static final String SERVER_URL = "http://169.48.179.82:8080";
-    public static final String API_KEY = "O4US6jESbVpia8SsBi9t3fyo-e2clabWKRb3njrsi3N9";
-}
